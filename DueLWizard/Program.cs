0a66a567e99624b38618dc623732102cf157ef96
flowerviaw <flow_kusuma_34pg@student.smktelkom-mlg.sch.id>
2026-02-06 15:09:27 +0700

    Menambah Class Wizard, dua object Wizard, dan simulasi battle

--- DueLWizard/Program.cs
+++ DueLWizard/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Wizard wizardA = new Wizard("Mak Lampir", 5);
+Wizard wizardB = new Wizard("Nirmala", 10);
+ 
+Console.WriteLine("Permainan Dimulai...\n");
+Console.WriteLine("Staistik Awal");
+wizardA.ShowStats();
+wizardB.ShowStats();
+
+wizardA.Attack(wizardB);
+wizardB.Attack(wizardA);
+wizardA.Attack(wizardB);
+
+Console.WriteLine("Permainan Selesai....\n");
+Console.WriteLine("Statistik Akhir");
+wizardA.ShowStats();
+wizardB.ShowStats();
+
+
+public class Wizard
+{
+    //deklarasi field 
+    public string Name;
+    public int Energy;
+    public int Damage;
+
+    //constructor
+
+    public Wizard(string name, int damage)
+    {
+        Name = name;
+        Energy = 100;
+        Damage = damage;
+    }
+
+    // deklarasi method 
+    public void ShowStats()
+    { 
+        Console.WriteLine($"Nmae: {Name}");
+        Console.WriteLine($"Energy: {Energy}\n");
+    }
+
+    public void Attack(Wizard enemyobj)
+    {
+        enemyobj.Energy -= Damage;
+        Console.WriteLine($"{Name}menyerang{enemyobj.Energy}");
+        Console.WriteLine($"Sisa energi {enemyobj.Name} {enemyobj.Energy}\n");
+
+    }
+
+
+}
