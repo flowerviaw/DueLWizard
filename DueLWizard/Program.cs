3f97659af8585874b7920acfc3c3488169d3d0b0
flowerviaw <flow_kusuma_34pg@student.smktelkom-mlg.sch.id>
2026-03-02 08:32:57 +0700

    menambah mekanisme pilihan aksi untuk bermain

--- DueLWizard/Program.cs
+++ DueLWizard/Program.cs
@@ -1,4 +1,6 @@
-﻿Wizard wizardA = new Wizard("Mak Lampir", 5);
+﻿using System.Threading.Channels;
+
+Wizard wizardA = new Wizard("Mak Lampir", 5);
 Wizard wizardB = new Wizard("Nirmala", 10);
  
 Console.WriteLine("Permainan Dimulai...\n");
@@ -6,9 +8,45 @@ Console.WriteLine("Staistik Awal");
 wizardA.ShowStats();
 wizardB.ShowStats();
 
-wizardA.Attack(wizardB);
-wizardB.Attack(wizardA);
-wizardA.Attack(wizardB);
+//wizardA.Attack(wizardB);
+//wizardB.Attack(wizardA);
+//wizardA.Attack(wizardB);
+//wizardA.Heal();
+
+string pilihan;
+while (true)
+{
+    Console.WriteLine($"1.  {wizardA.Name} menyerang {wizardB.Name}");
+    Console.WriteLine($"2.  {wizardB.Name} menyerang {wizardA.Name}");
+    Console.WriteLine($"{wizardA.Name} Melakukan Heal");
+    Console.WriteLine($"{wizardB.Name} Melakukan Heal");
+
+    Console.WriteLine("\nPilihan myu (1/2/3/4)");
+    pilihan = Console.ReadLine();
+
+    if (pilihan == "1") wizardA.Attack(wizardB);
+    else if (pilihan == "2") wizardB.Attack(wizardA);
+    else if (pilihan == "3") wizardA.Heal();
+    else if (pilihan == "4") wizardB.Heal();
+
+    if (wizardA.Energy <= 0 || wizardB.Energy <= 0)
+    {
+        Console.WriteLine("Permainan berakhir!");
+        if (wizardA.Energy > wizardB.Energy)
+        {
+            Console.WriteLine($"{wizardB.Name} berhasil dikalahkan");
+            Console.WriteLine($"{wizardA.Name} keluar sebagai pemenangnya!");
+        }
+        else
+        {
+            Console.WriteLine($"{wizardA.Name} berhasil dikalahkan!");
+            Console.WriteLine($"{wizardB.Name} keluar sebagai pemenangnya!");
+        }
+        break;
+    }
+
+
+}
 
 Console.WriteLine("Permainan Selesai....\n");
 Console.WriteLine("Statistik Akhir");
